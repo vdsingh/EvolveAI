bb6d518cbaf9a959ebfadbe71837133ce85568f1
Vikram Singh <vik.devmail@gmail.com>
2023-01-06 19:34:55 -0800

    Add realm implementation in view controller + project files

--- EvolveAI/Controllers/ViewController.swift
+++ EvolveAI/Controllers/ViewController.swift
@@ -6,15 +6,21 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
     
     var prompt: String = ""
     var numDays: Int = 0
     
+    let realm = try! Realm()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let results = realm.objects(EAGoal.self)
+        print("Results: \(results)")
+
         let questions: [EAFormQuestionViewModel] = [
             EAFormQuestionViewModel(
                 question: "I want to:",
@@ -31,7 +37,7 @@ class ViewController: UIViewController {
                             print("Edited days: \(self?.numDays)")
                         }))
         ]
-        
+
         let subview = EAFormView(questions: questions)
         subview.backgroundColor = .yellow
         subview.translatesAutoresizingMaskIntoConstraints = false
@@ -55,21 +61,52 @@ class ViewController: UIViewController {
             executeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             executeButton.heightAnchor.constraint(equalToConstant: 40),
         ])
-//        view =
         
         
     }
     
     @objc func executeRequest() {
+        let request = EAOpenAIRequest.completionsRequest(
+            prompt: EAGoal.createOpenAICompletionsRequestString(goal: self.prompt, numDays: self.numDays)
+        )
+
+        EAService.shared.execute(request, expecting: EAOpenAICompletionsResponse.self, completion: { [weak self] (result) in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            switch result {
+            case .success(let object):
+                print("$Log: \(object)")
+                let goal = EAGoal(goal: strongSelf.prompt,
+                                  numDays: 10,
+                                  apiResponse: object)
+                
+                DispatchQueue.main.async {
+                    do {
+                        try? strongSelf.realm.write {
+                            strongSelf.realm.add(goal)
+                        }
+                    }
+                    strongSelf.displayText(text: goal.aiResponse)
+                    print("Set the label text to \(goal.aiResponse)")
+                }
+            case .failure(let error):
+                print("$Error: \(String(describing: error))")
+            }
+        })
+    }
+    
+    func displayText(text: String) {
         let label = UILabel()
         label.backgroundColor = .yellow
         label.font = .systemFont(ofSize: 14)
-        label.text = "hello"
+        label.text = text
         label.textColor = .black
         label.textAlignment = .left
         label.numberOfLines = 0
-
         label.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(label)
 
         NSLayoutConstraint.activate([
@@ -78,31 +115,5 @@ class ViewController: UIViewController {
             label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
-
-        let request = EAOpenAIRequest.completionsRequest(
-            prompt: EAGoal.createOpenAICompletionsRequestString(goal: self.prompt, numDays: self.numDays)
-        )
-
-        EAService.shared.execute(request, expecting: EAOpenAICompletionsResponse.self, completion: { result in
-            switch result {
-            case .success(let object):
-                print("$Log: \(object)")
-                let goal = EAGoal(goal: self.prompt,
-                                  numDays: 10,
-                                  apiResponse: object)
-                DispatchQueue.main.async {
-                    label.text = goal.aiResponse
-                    print("Set the label text to \(goal.aiResponse)")
-                }
-            case .failure(let error):
-                print("$Error: \(String(describing: error))")
-            }
-        })
-    }
-}
-
-extension ViewController: UITextFieldDelegate {
-    func textFieldDidEndEditing(_ textField: UITextField) {
-//        textField.
     }
 }
