d4fd46debc5adc81bac93465335e11138f66f336
Vikram Singh <vik.devmail@gmail.com>
2023-01-07 22:29:28 -0800

    Create singleton class for Mocking data and implement function for mocking goals

---
+++ EvolveAI/Utility/Mocking.swift
@@ -0,0 +1,35 @@
+//
+//  Mocking.swift
+//  EvolveAI
+//
+//  Created by Vikram Singh on 1/7/23.
+//
+
+import Foundation
+class Mocking {
+    static let shared = Mocking()
+    
+    private init() { }
+    
+    public func createMockGoals(numGoals: Int) -> [EAGoal] {
+        let mockGoalsMap: [String: String] = [
+            "learn the violin": "Day 1: Buy a violin.",
+            "read 3 books": "Day 1: Buy a book.",
+            "lose 10 pounds": "Day 1: Create a diet plan.",
+            "learn to cook": "Day 1: Buy kitchen supplies",
+        ]
+        
+        var goals = [EAGoal]()
+        for _ in 0..<numGoals {
+            guard let randomGoal = mockGoalsMap.randomElement() else {
+                fatalError("$Error: no mock goals")
+            }
+            let goal = EAGoal(goal: randomGoal.key,
+                              numDays: Int.random(in: 1...30),
+                              aiResponse: randomGoal.value)
+            goals.append(goal)
+        }
+        
+        return goals
+    }
+}
