27891caadd9f8a6f9eb0ac94862222e0e2d0569f
Vikram Singh <vik.devmail@gmail.com>
2023-01-07 19:47:52 -0800

    Implement constants, CreateGoalCode enum, realm access, and private initializer

--- EvolveAI/Repository/EAGoalsService.swift
+++ EvolveAI/Repository/EAGoalsService.swift
@@ -8,6 +8,42 @@
 import Foundation
 /// API for goals data
 class EAGoalsService {
+    /// Shared instance of the goals service
+    static let shared = EAGoalsService()
+    /// Access to the Realm database
+    let realm = try! Realm()
+    
+    /// Constants that are used in the goals service
+    struct Constants {
+        static let characterLimit = 300
+        static let numDaysLimit = 30
+    }
+    
+    /// Possible codes to be returned by the create goal function
+    enum CreateGoalCode: Int {
+        /// Goal was created successfully
+        case success = 0
+        /// Proposed number of days was higher than the limit
+        case dayLimitExceeded = 1
+        /// Some other error was encountered
+        case error = 2
+        
+        /// Provides a description of the CreateGoalCode
+        /// - Returns: a description of the CreateGoalCode
+        func codeDescription() -> String {
+            switch self {
+            case .success:
+                return "Success"
+            case .dayLimitExceeded:
+                return "Please choose a shorter number of days to achieve the goal. The limit is \(Constants.numDaysLimit)"
+            case .error:
+                return "Other error encountered"
+            }
+        }
+    }
+    
+    /// Private initializer ensures that the shared instance is used
+    private init() { }
     
     /// Creates a string to send to the OpenAI Completions endpoint
     /// - Parameters:
