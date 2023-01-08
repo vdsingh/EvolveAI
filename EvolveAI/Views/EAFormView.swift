cf0004a6267056b0e9127c6a3967d6970364da63
Vikram Singh <vik.devmail@gmail.com>
2023-01-07 22:43:36 -0800

    Add docstrings and clean up EAFormView

--- EvolveAI/Views/EAFormView.swift
+++ EvolveAI/Views/EAFormView.swift
@@ -7,45 +7,60 @@
 
 import Foundation
 import UIKit
+/// View for Forms
 class EAFormView: UIView {
+    /// The UIViews for the questions
+    var questionViews: [UIView]
     
-    var questions: [UIView]
-    private let padding: CGFloat = 20
-    private let spacing: CGFloat = 20
+    /// The constants used in this View
+    struct Constants {
+        /// The padding for the questions stack
+        static let padding: CGFloat = 20
+        /// The spacing between questions
+        static let spacing: CGFloat = 20
+    }
     
+    /// A map for the callback functions for when textfield has been edited
     private var textfieldCallbackGraph: [UITextField: ((_ text: String) -> Void)] = [:]
     
-    init(questions: [EAFormQuestionViewModel]) {
-        self.questions = []
+    /// ViewModel initializer
+    /// - Parameter viewModels: The EAFormQuestionViewModels to instantiate the View
+    init(questionViewModels: [EAFormQuestionViewModel]) {
+        self.questionViews = []
         
         super.init(frame: .zero)
         self.backgroundColor = .yellow
 
-        self.setupUI(questions: questions)
+        self.addViewsAndEstablishConstraints(questionViewModels: questionViewModels)
     }
     
-    private func setupUI(questions: [EAFormQuestionViewModel]) {
-        let stack = constructQuestionsStackView(questions: questions)
-        stack.spacing = spacing
+    /// Add the subviews to the view and establish constraints
+    /// - Parameter questions: The ViewModels which specify the subviews to add
+    private func addViewsAndEstablishConstraints(questionViewModels: [EAFormQuestionViewModel]) {
+        let stack = constructQuestionsStackView(questionViewModels: questionViewModels)
+        stack.spacing = Constants.spacing
         addSubview(stack)
         
         NSLayoutConstraint.activate([
-            stack.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: padding),
-            stack.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor, constant: -padding),
-            stack.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: padding),
+            stack.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: Constants.padding),
+            stack.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor, constant: -Constants.padding),
+            stack.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: Constants.padding),
         ])
     }
     
-    private func constructQuestionsStackView(questions: [EAFormQuestionViewModel]) -> UIStackView {
+    /// Constructs a UIStackView that contains all of the question views
+    /// - Parameter questionViewModels: the viewModels that represent the forms questions
+    /// - Returns: a UIStackView that contains all of the question views
+    private func constructQuestionsStackView(questionViewModels: [EAFormQuestionViewModel]) -> UIStackView {
         let questionStack = UIStackView()
         questionStack.translatesAutoresizingMaskIntoConstraints = false
         questionStack.axis = .vertical
         
         
-        for questionViewModel in questions {
+        for questionViewModel in questionViewModels {
             let view = EAFormQuestionView(viewModel: questionViewModel)
             view.translatesAutoresizingMaskIntoConstraints = false
-            self.questions.append(view)
+            self.questionViews.append(view)
             questionStack.addArrangedSubview(view)
             
             switch questionViewModel.questionResponse {
@@ -67,10 +82,14 @@ class EAFormView: UIView {
 }
 
 extension EAFormView: UITextFieldDelegate {
+    /// Handles UITextFieldDidEndEditing for questions whose response is a textfield
+    /// - Parameter textField: the textfield in question
     func textFieldDidEndEditing(_ textField: UITextField) {
+        // Find the callback function for the specified textfield
         guard let callback = textfieldCallbackGraph[textField] else {
             return
         }
+        // Call the callback with the textfield's text.
         callback(textField.text ?? "")
     }
 }
