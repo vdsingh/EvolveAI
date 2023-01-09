d2ac5562eda05066818e3c47bbb2c17826776013
Vikram Singh <vik.devmail@gmail.com>
2023-01-08 23:00:29 -0800

    Implement loadView for Form VC

--- EvolveAI/Controllers/EAGoalCreationFormViewController.swift
+++ EvolveAI/Controllers/EAGoalCreationFormViewController.swift
@@ -12,6 +12,16 @@ class EAGoalCreationFormViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    override func loadView() {
+        let view = EAFormView(questions: [
+            .textfield(question: "Question", textfieldPlaceholder: "Placeholder", textFieldWasEdited: { text in
+                print("Text was edited! \(text)")
+            })
+        ])
+        
+        self.view = view
+    }
+    
     required init?(coder: NSCoder) {
         return nil
     }
