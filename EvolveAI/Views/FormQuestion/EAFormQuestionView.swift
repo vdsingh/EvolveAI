2f8a5bd6129d287979d14a2356134f264a3e455c
Vikram Singh <vik.devmail@gmail.com>
2023-01-08 22:59:25 -0800

    Replace FormQuestionView with a protocol because FormQuestions are less structured than anticipated (and each need their own custom View)

---
+++ EvolveAI/Views/FormQuestion/EAFormQuestionView.swift
@@ -0,0 +1,12 @@
+////
+////  FormQuestionView.swift
+////  EvolveAI
+////
+////  Created by Vikram Singh on 1/5/23.
+////
+//
+import Foundation
+import UIKit
+protocol EAFormQuestionView: UIView {
+    var requiredHeight: CGFloat { get }
+}
