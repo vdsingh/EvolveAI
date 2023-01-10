936ee5320530626bac4d9cb42fdd135eb252048c
Vikram Singh <vik.devmail@gmail.com>
2023-01-09 18:48:33 -0800

    Fix layout constraint conflicts, lint, and add docstrings to TextfieldQuestionView

--- EvolveAI/Views/FormQuestion/EATextfieldQuestionView.swift
+++ EvolveAI/Views/FormQuestion/EATextfieldQuestionView.swift
@@ -7,16 +7,20 @@
 
 import Foundation
 import UIKit
+/// View for basic TextField Questions
 class EATextfieldQuestionView: UIStackView, EAFormQuestionView {
+    /// The required height for this view
     var requiredHeight: CGFloat = 80
     
+    /// Label that displays the question
     let questionLabel: UILabel = {
         let questionLabel = UILabel()
         questionLabel.translatesAutoresizingMaskIntoConstraints = false
-        questionLabel.font = .systemFont(ofSize: Constants.bodyFontSize, weight: .medium)
+        questionLabel.font = .systemFont(ofSize: EAFontSize.bodyFontSize.rawValue, weight: .medium)
         return questionLabel
     }()
     
+    /// TextField where user enters response
     let textfield: UITextField = {
         let textfield = UITextField()
         textfield.translatesAutoresizingMaskIntoConstraints = false
@@ -25,31 +29,33 @@ class EATextfieldQuestionView: UIStackView, EAFormQuestionView {
         return textfield
     }()
     
+    /// ViewModel initializer
+    /// - Parameter viewModel: The ViewModel used to set the data of the View
     init(viewModel: EATextfieldQuestionViewModel) {
         super.init(frame: .zero)
         
-        translatesAutoresizingMaskIntoConstraints = false
-        distribution = .fillEqually
-        alignment = .fill
-        axis = .vertical
-        backgroundColor = .green
-        spacing = 5
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.distribution = .fillProportionally
+        self.alignment = .fill
+        self.axis = .vertical
+        self.backgroundColor = .green
+        self.spacing = 5
         
         self.questionLabel.text = viewModel.question
         self.textfield.placeholder = viewModel.responsePlaceholder
-        addSubviewsAndEstablishConstraints()
+        self.addSubviewsAndEstablishConstraints()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
     }
     
+    /// Add the subviews to the view and establish constraints
     private func addSubviewsAndEstablishConstraints() {
-        addArrangedSubview(questionLabel)
-        addArrangedSubview(textfield)
+        self.addArrangedSubview(questionLabel)
+        self.addArrangedSubview(textfield)
         
         NSLayoutConstraint.activate([
-            questionLabel.heightAnchor.constraint(equalToConstant: Constants.bodyFontSize),
             textfield.heightAnchor.constraint(equalToConstant: 40),
         ])
     }
