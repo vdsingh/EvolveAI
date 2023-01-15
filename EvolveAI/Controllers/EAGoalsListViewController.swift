e2804b2a9312ca8e10d6ac7b812ec9fca6c5927b
Vikram Singh <vik.devmail@gmail.com>
2023-01-15 05:46:25 -0800

    Add goals collectionview logic to VC. Add debug statements for debugGoalsList flag.

--- EvolveAI/Controllers/EAGoalsListViewController.swift
+++ EvolveAI/Controllers/EAGoalsListViewController.swift
@@ -17,6 +17,7 @@ class EAGoalsListViewController: UIViewController {
         self.navigator = navigator
         self.goals = goals
         super.init(nibName: nil, bundle: nil)
+        self.printDebug("Set goals to \(self.goals.compactMap{ $0.getSimplifiedDescription() + "\n"})")
         self.title = "Goals List"
     }
     
@@ -39,6 +40,7 @@ class EAGoalsListViewController: UIViewController {
         }
         
         self.goals = EAGoalsService.shared.getAllPersistedGoals()
+        printDebug("Set goals to \(self.goals.compactMap( {$0.getSimplifiedDescription() + "\n"} ))")
         self.getView().refreshView()
     }
     
@@ -49,8 +51,9 @@ class EAGoalsListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.getView().setTableViewDelegate(self)
-        self.getView().setTableViewDataSource(self)
+        self.getView().setCollectionViewDataSource(self)
+        self.getView().setCollectionViewDelegate(self)
+        printDebug("Set delegate and datasource.")
     }
     
     /// Function called when the user clicks the question button (max goal limit reached)
@@ -68,6 +71,7 @@ class EAGoalsListViewController: UIViewController {
     /// Function called when the user clicks the add button to create a new goal
     @objc private func addGoalButtonPressed() {
         self.navigator.navigate(to: .createGoal(goalWasCreated: { [weak self] in
+            self?.printDebug("Goal was created from form. Refreshing goals data and view.")
             self?.goals = EAGoalsService.shared.getAllPersistedGoals()
             self?.getView().refreshView()
         }))
@@ -83,31 +87,29 @@ class EAGoalsListViewController: UIViewController {
         }
     }
     
+    private func printDebug(_ message: String) {
+        if(Flags.debugGoalsList) {
+            print("$Log: \(message)")
+        }
+    }
+    
     required init?(coder: NSCoder) {
         return nil
     }
 }
 
-// MARK: - TableView Delegate
-extension EAGoalsListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let goal = goals[indexPath.row]
-        navigator.navigate(to: .viewGoal(goal: goal))
-    }
-}
-
-// MARK: - TableView DataSource
-extension EAGoalsListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+extension EAGoalsListViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return goals.count
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        printDebug("Number of items: \(self.goals.count)")
+        return self.goals.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: EAGoalTableViewCell.reuseIdentifier, for: indexPath) as? EAGoalTableViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: EAGoalCollectionViewCell.reuseIdentifier, for: indexPath) as? EAGoalCollectionViewCell {
             let goal = goals[indexPath.row]
             let goalViewModel = EAGoalViewModel(
                 title: goal.goal,
@@ -116,10 +118,54 @@ extension EAGoalsListViewController: UITableViewDataSource {
                 additionalDetails: goal.additionalDetails
             )
             cell.configure(with: goalViewModel)
+            self.printDebug("returned cell and configured with \(goalViewModel) at \(indexPath)")
             return cell
         }
         
         print("$Error: EAGoalTableViewCell couldn't be dequeued correctly.")
-        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
     }
 }
+
+extension EAGoalsListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let goal = goals[indexPath.row]
+        navigator.navigate(to: .viewGoal(goal: goal))
+    }
+}
+
+// MARK: - TableView Delegate
+//extension EAGoalsListViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let goal = goals[indexPath.row]
+//        navigator.navigate(to: .viewGoal(goal: goal))
+//    }
+//}
+//
+//// MARK: - TableView DataSource
+//extension EAGoalsListViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 60
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return goals.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: EAGoalTableViewCell.reuseIdentifier, for: indexPath) as? EAGoalTableViewCell {
+//            let goal = goals[indexPath.row]
+//            let goalViewModel = EAGoalViewModel(
+//                title: goal.goal,
+//                numDays: goal.numDays,
+//                dayGuides: goal.dayGuides,
+//                additionalDetails: goal.additionalDetails
+//            )
+//            cell.configure(with: goalViewModel)
+//            return cell
+//        }
+//
+//        print("$Error: EAGoalTableViewCell couldn't be dequeued correctly.")
+//        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//    }
+//}
