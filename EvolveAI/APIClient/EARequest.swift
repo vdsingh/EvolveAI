f022b3fda28be9d27bbbecbdff77b7bb22e25ab8
Vikram Singh <vik.devmail@gmail.com>
2023-01-23 14:30:09 -0800

    Add httpMethod to EARequest protocol

--- EvolveAI/APIClient/EARequest.swift
+++ EvolveAI/APIClient/EARequest.swift
@@ -10,4 +10,5 @@ import Foundation
 /// Protocol that all EARequests must follow to be used in the API Service layer
 protocol EARequest {
     var urlRequest: URLRequest? { get }
+    var httpMethod: HTTPMethod { get }
 }
