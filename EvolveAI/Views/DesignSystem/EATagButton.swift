ca7b0a0da7ec18f9233872ed79ad1cb18697895d
Vikram Singh <vik.devmail@gmail.com>
2023-02-10 22:51:25 -0500

    TagButton docstrings

--- EvolveAI/Views/DesignSystem/EATagButton.swift
+++ EvolveAI/Views/DesignSystem/EATagButton.swift
@@ -7,13 +7,19 @@
 
 import Foundation
 import UIKit
-final class EATagButton: UIButton {
 
+/// Custom Button View to represent goal tags
+final class EATagButton: UIButton {
+    
+    /// Normal initializer
+    /// - Parameter tag: The name of the tag (ex: "Fitness")
     init(tag: String) {
         super.init(frame: .zero)
         self.setUIProperties(tag: tag)
     }
-
+    
+    /// Sets the UI properties for this View
+    /// - Parameter tag: The name for the tag
     private func setUIProperties(tag: String) {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.setTitle(tag, for: .normal)
