f967b6e943bdf31572830b7bb8b357c929fcae2e
Vikram Singh <vik.devmail@gmail.com>
2023-02-10 22:51:10 -0500

    StackView docstrings

--- EvolveAI/Views/DesignSystem/EAStackView.swift
+++ EvolveAI/Views/DesignSystem/EAStackView.swift
@@ -8,9 +8,16 @@
 import Foundation
 import UIKit
 
+/// Custom StackView element
 final class EAStackView: UIStackView, EAFormElementView {
+    
+    /// The height required for this UI element
     var requiredHeight: CGFloat
-
+    
+    /// Normal initializer
+    /// - Parameters:
+    ///   - axis: The axis for this StackView
+    ///   - subViews: The views contained within this StackView
     init(axis: NSLayoutConstraint.Axis, subViews: [EAFormElementView]) {
         var calculatedHeight: CGFloat = 0
         if axis == .vertical {
@@ -24,18 +31,21 @@ final class EAStackView: UIStackView, EAFormElementView {
         }
 
         self.requiredHeight = calculatedHeight
-
         super.init(frame: .zero)
         self.setUIProperties(axis: axis)
         self.addSubviews(subViews: subViews)
     }
-
+    
+    /// Adds sub views to this StackView
+    /// - Parameter subViews: The sub views to add to this StackView
     private func addSubviews(subViews: [EAFormElementView]) {
         for subView in subViews {
             self.addArrangedSubview(subView)
         }
     }
-
+    
+    /// Sets the UI properties for this StackView
+    /// - Parameter axis: The axis for this StackView
     private func setUIProperties(axis: NSLayoutConstraint.Axis) {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.axis = axis
