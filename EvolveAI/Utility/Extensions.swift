96445c385daf30ed5d19acf6af69b817188165dc
Vikram Singh <vik.devmail@gmail.com>
2023-02-26 02:39:38 -0500

    UIScrollView extension allows ScrollViews to scroll to specific SubViews

--- EvolveAI/Utility/Extensions.swift
+++ EvolveAI/Utility/Extensions.swift
@@ -103,3 +103,32 @@ extension Date {
         paramDateComponents.day == selfDateComponents.day
     }
 }
+
+extension UIScrollView {
+
+    // Scroll to a specific view so that it's top is at the top our scrollview
+    func scrollToView(view: UIView, animated: Bool, additionalOffset: CGFloat) {
+        if let origin = view.superview {
+            // Get the Y position of your child view
+            let childStartPoint = origin.convert(view.frame.origin, to: self)
+            // Scroll to a rectangle starting at the Y of your subview, with a height of the scrollview
+            self.scrollRectToVisible(CGRect(x: 0, y: childStartPoint.y, width: 1, height: self.frame.height - additionalOffset), animated: animated)
+        }
+    }
+
+    // Bonus: Scroll to top
+    func scrollToTop(animated: Bool) {
+        let topOffset = CGPoint(x: 0, y: -contentInset.top)
+        setContentOffset(topOffset, animated: animated)
+    }
+
+    // Bonus: Scroll to bottom
+    func scrollToBottom() {
+        print("SCROLLING TO BOTTOM: contentSize height: \(contentSize.height). Bounds height: \(bounds.size.height). ContentInset bottom: \(contentInset.bottom)")
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height + contentInset.bottom)
+        if bottomOffset.y > 0 {
+            setContentOffset(bottomOffset, animated: true)
+        }
+    }
+
+}
