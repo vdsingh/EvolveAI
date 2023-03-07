f4278da8ec24863f0a777f69d9fbc85ae4db6310
Vikram Singh <vik.devmail@gmail.com>
2023-03-06 21:07:22 -0500

    Make EAEndpoint an enum. Associated values for the specific endpoint enum

--- EvolveAI/APIClient/EAEndpoint.swift
+++ EvolveAI/APIClient/EAEndpoint.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 /// Protocol that all endpoints must follow
-protocol EAEndpoint {
+// protocol EAEndpoint {
+//    //TODO: Docstrings
+//    var rawVal: String { get }
+// }
 
+enum EAEndpoint {
+    case EAOpenAIEndpoint(endpoint: EAOpenAIEndpoint)
+    case EAMockingEndpoint(endpoint: EAMockingEndpoint)
+
+    var rawVal: String {
+        switch self {
+        case .EAOpenAIEndpoint(let endpoint):
+            return endpoint.rawValue
+
+        case .EAMockingEndpoint(let endpoint):
+            return endpoint.rawValue
+        }
+    }
 }
