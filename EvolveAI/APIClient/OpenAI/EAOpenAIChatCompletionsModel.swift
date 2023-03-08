f6f71103b830c535e65d683681b6d2fb3e244346
Vikram Singh <vik.devmail@gmail.com>
2023-03-07 20:43:40 -0500

    Implement EAGoalCreationModel enum, Create EAMockingModel, EAOpenAIChatCompletionsModel enums.

--- EvolveAI/APIClient/OpenAI/EAOpenAIChatCompletionsModel.swift
+++ EvolveAI/APIClient/OpenAI/EAOpenAIChatCompletionsModel.swift
@@ -8,22 +8,55 @@
 import Foundation
 import RealmSwift
 
-protocol EAOpenAIModel {
-//    func getTokenLimit() -> Int
-    var tokenLimit: Int { get }
-    
-//    var modelType: EAOpenAIModel.Type { get }
-    var rawVal: String { get }
+// TODO: Docstring
+// protocol EAGoalCreationModel {
+//    var tokenLimit: Int { get }
+//
+//    var rawVal: String { get }
+// }
+
+// TODO: Docstring
+enum EAGoalCreationModel {
+    case EAOpenAIChatCompletionsModel(model: EAOpenAIChatCompletionsModel)
+    case EAOpenAICompletionsModel(model: EAOpenAICompletionsModel)
+    case EAMockingModel(model: EAMockingModel)
+
+    var rawVal: String {
+        switch self {
+        case .EAOpenAICompletionsModel(let model):
+            return model.rawValue
+
+        case .EAOpenAIChatCompletionsModel(let model):
+            return model.rawValue
+
+        case .EAMockingModel(let model):
+            return model.rawValue
+        }
+    }
 }
 
-//extension EAOpenAIModel {
 // TODO: Docstring
-enum EAOpenAIChatCompletionsModel: String, Codable, EAOpenAIModel {
-//    var modelType: EAOpenAIModel.Type { return EAOpenAIChatCompletionsModel.self }
-    
+protocol EAGoalCreationModelProtocol {
+    var tokenLimit: Int { get }
+}
+
+// TODO: Docstring
+enum EAMockingModel: String {
+
+    case mocked
+
+    var tokenLimit: Int {
+        4000
+    }
+}
+
+// extension EAOpenAIModel {
+// TODO: Docstring
+enum EAOpenAIChatCompletionsModel: String, Codable, EAGoalCreationModelProtocol {
+
     /// Most capable GPT-3.5 model and optimized for chat at 1/10th the cost of text-davinci-003
     case gpt3_5Turbo = "gpt-3.5-turbo"
-    
+
     /// Gets the max number of tokens that a model can respond with
     /// - Returns: The number of tokens than a model can respond with
     var tokenLimit: Int {
