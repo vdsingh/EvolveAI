b572a6a35da8495b719e0dc021fe92958eea5c80
Vikram Singh <vik.devmail@gmail.com>
2023-03-09 13:18:34 -0500

    Docstrings, remove old code

--- EvolveAI/APIClient/OpenAI/EAOpenAIChatCompletionsModel.swift
+++ EvolveAI/APIClient/OpenAI/EAOpenAIChatCompletionsModel.swift
@@ -8,19 +8,19 @@
 import Foundation
 import RealmSwift
 
-// TODO: Docstring
-// protocol EAGoalCreationModel {
-//    var tokenLimit: Int { get }
-//
-//    var rawVal: String { get }
-// }
-
-// TODO: Docstring
+/// Represents groupings of models that can be used for goal creations
 enum EAGoalCreationModel {
-    case EAOpenAIChatCompletionsModel(model: EAOpenAIChatCompletionsModel)
-    case EAOpenAICompletionsModel(model: EAOpenAICompletionsModel)
-    case EAMockingModel(model: EAMockingModel)
 
+    /// An OpenAI's Chat Completions Model
+    case EAOpenAIChatCompletionsModel(model: EAOpenAIChatCompletionsModel)
+
+    /// An OpenAI's Completions Model
+    case EAOpenAICompletionsModel(model: EAOpenAICompletionsModel)
+
+    /// Mock Model
+    case EAMockingModel(model: EAMockingGoalCreationModel)
+
+    /// The Raw Value for the model
     var rawVal: String {
         switch self {
         case .EAOpenAICompletionsModel(let model):
@@ -35,30 +35,32 @@ enum EAGoalCreationModel {
     }
 }
 
-// TODO: Docstring
+/// Protocol that any Goal Creation Models must follow
 protocol EAGoalCreationModelProtocol {
+
+    /// The token limit for a model
     var tokenLimit: Int { get }
 }
 
-// TODO: Docstring
-enum EAMockingModel: String {
+/// Mock Goal Creation Model
+enum EAMockingGoalCreationModel: String {
 
+    /// The goal creation was created by mocking
     case mocked
 
+    /// The max number of tokens that a model can respond with
     var tokenLimit: Int {
         4000
     }
 }
 
-// extension EAOpenAIModel {
-// TODO: Docstring
+/// Open AI Chat Completions Models
 enum EAOpenAIChatCompletionsModel: String, Codable, EAGoalCreationModelProtocol {
 
     /// Most capable GPT-3.5 model and optimized for chat at 1/10th the cost of text-davinci-003
     case gpt3_5Turbo = "gpt-3.5-turbo"
 
-    /// Gets the max number of tokens that a model can respond with
-    /// - Returns: The number of tokens than a model can respond with
+    /// The max number of tokens that a model can respond with
     var tokenLimit: Int {
         switch self {
         case .gpt3_5Turbo:
