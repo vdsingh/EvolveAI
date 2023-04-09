643e75aef6244d84fcd6db61ce5ff43c9345c9e4
Vikram Singh <vik.devmail@gmail.com>
2023-04-09 19:50:29 -0400

    Implement Language Model Service: decouple Open AI Language Models from EvolveAI specific models.

---
+++ EvolveAI/Repository/EAOpenAILanguageModelService.swift
@@ -0,0 +1,79 @@
+//
+//  EAOpenAIService.swift
+//  EvolveAI
+//
+//  Created by Vikram Singh on 4/4/23.
+//
+
+import Foundation
+import RealmSwift
+
+class EAOpenAILanguageModelService: Debuggable {
+    
+    //TODO: Docstring
+    let debug = false
+    
+    //TODO: Docstring
+    enum EAOpenAILanguageModelServiceError: Error {
+        case responseHadNoChoices
+        case nilRequest
+    }
+
+    //TODO: Docstring
+    static let shared = EAOpenAILanguageModelService()
+    
+    //TODO: Docstring
+    var realm: Realm {
+        do {
+            return try Realm()
+        } catch {
+            fatalError("$Error: Realm is nil.")
+        }
+    }
+
+    /// Creates a goal and persists it to the Realm Database
+    /// - Parameters:
+    ///   - goal: The goal that user is trying to achieve (ex: "learn the violin")
+    ///   - numDays: The number of days that the goal is to be achieved by (ex: 30 days)
+    func executeCompletionsRequest(
+        model: EAOpenAICompletionsModel,
+        prompt: String,
+        completion: @escaping (Result<String, Error>) -> Void
+    ) {
+        guard let request = EAOpenAIRequest.completionsRequest(
+            model: model,
+            prompt: prompt,
+            maxTokens: model.tokenLimit
+        ) else {
+            print("$Error: Attempted to execute completions request but request was nil")
+            completion(.failure(EAOpenAILanguageModelServiceError.nilRequest))
+            return
+        }
+
+        EARestAPIService.shared.execute(
+            request,
+            expecting: EAOpenAICompletionsResponse.self,
+            completion: { result in
+                switch result {
+                case .success(let aiCompletionsResponse):
+                    if let first = aiCompletionsResponse.getChoices().first {
+                        let aiResponse = first.getLastText()
+                        completion(.success(aiResponse))
+                    } else {
+                        completion(.failure(EAOpenAILanguageModelServiceError.responseHadNoChoices))
+                    }
+
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        )
+    }
+    
+    //TODO: Docstring
+    func printDebug(_ message: String) {
+        if self.debug {
+            print("$Log: \(message)")
+        }
+    }
+}
