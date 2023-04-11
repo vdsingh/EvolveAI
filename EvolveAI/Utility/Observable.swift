be0136ba9605aef5fd20fb6beeee3efcff5f0fd4
Vikram Singh <vik.devmail@gmail.com>
2023-04-10 20:52:16 -0400

    Implement RequiredObservable class for Observable objects with non-nil values.

--- EvolveAI/Utility/Observable.swift
+++ EvolveAI/Utility/Observable.swift
@@ -7,30 +7,80 @@
 
 import Foundation
 
+class RequiredObservable<T>: Debuggable {
+    let debug = false
+    
+    let label: String
+    
+    var value: T {
+        didSet {
+            self.didSetValue()
+        }
+    }
+    
+    init(_ value: T, label: String) {
+        self.value = value
+        self.label = label
+        printDebug("initialized Observable with label: [\(label)]. value: [\(value)]")
+        self.didSetValue()
+    }
+    
+    private var listener: ((T) -> Void)?
+    
+    private func didSetValue() {
+        printDebug("value of observable [\(label)] was changed to [\(String(describing: value))]")
+        self.listener?(self.value)
+    }
+    
+    func bind(_ listener: @escaping (T) -> Void) {
+        printDebug("binded Observable with label [\(label)]")
+        listener(self.value)
+        self.listener = listener
+    }
+    
+    func printDebug(_ message: String) {
+        if self.debug {
+            print("$Log (Observable): \(message)")
+        }
+    }
+}
+
 //TODO: Docstrings
-class Observable<T> {
+class Observable<T>: Debuggable {
+    
+    let debug = false
     
     let label: String
     
     var value: T? {
         didSet {
-            print("$Log: value of observable [\(label)] was changed to [\(String(describing: value))]")
-//            DispatchQueue.main.async {
-                self.listener?(self.value)
-//            }
+            self.didSetValue()
         }
     }
     
     init(_ value: T?, label: String) {
         self.value = value
         self.label = label
+        printDebug("initialized Observable with label: [\(label)]. value: [\(value)]")
+        self.didSetValue()
     }
     
     private var listener: ((T?) -> Void)?
     
+    private func didSetValue() {
+        printDebug("value of observable [\(label)] was changed to [\(String(describing: value))]")
+        self.listener?(self.value)
+    }
+    
     func bind(_ listener: @escaping (T?) -> Void) {
+        printDebug("binded Observable with label [\(label)]")
         listener(self.value)
         self.listener = listener
     }
     
+    func printDebug(_ message: String) {
+        if self.debug {
+            print("$Log (Observable): \(message)")
+        }
+    }
 }
